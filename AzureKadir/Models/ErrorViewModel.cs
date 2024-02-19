36553517cd8bb8d0952a9b5998f3bd4bea7deeef
Kadir Kemal Yörük <kadir.yoruk@hotmail.com>
2024-02-19 18:55:40 +0300

    first

---
+++ AzureKadir/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureKadir.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
